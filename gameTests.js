b134171e35f430b6f7190a79b760cee7ca8bdd4e
Allen Tuggle <allen.tuggle@excella.com>
2020-12-01 11:55:05 -0500

    Implemented can bowl all gutter game test

--- gameTests.js
+++ gameTests.js
@@ -4,6 +4,14 @@ const { Game } = require("./game.js");
 describe('Tests', () => {
     it('CanCreateGameObject', () => {
         let myGame = new Game();
-        assert(myGame);
+        assert(myGame, 'Game object was not successfully created');
+    }),    
+    it('Can Bowl gutter game (all zeros)', () => {
+        let myGame = new Game();
+        for (roll = 0; roll < 10; roll++) {
+            myGame.bowl(0);
+        }
+        
+        assert(0 === myGame.scoreGame(), 'Invalid score calculated');
     })
 });
